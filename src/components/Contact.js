4552fea01915a0a2f92e89953d74586e1a17e5c7
Abhishekjoshi98 <98544171+Abhishekjoshi98@users.noreply.github.com>
2022-09-17 12:38:08 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,8 @@ const Contact = _ =>{
     return (
         <section>
         <h3>📞Contact Me</h3>
-        <a href="https://www.linkedin.com/in/abhishek-joshi-481a8720a/">LinkedIn</a>
-        <a href="https://github.com/Abhishekjoshi98">Github</a>
-        <a href="#">Instagram</a>
+        <a href="https://www.linkedin.com/in/abhishek-joshi-481a8720a/" target='_blank' rel='noreferrer'>LinkedIn</a>
+        <a href="https://github.com/Abhishekjoshi98" target='_blank' rel='noreferrer'>Github</a>
         </section>
     )
 }
